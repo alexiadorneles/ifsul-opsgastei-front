23a4dd21309cd041702b1412bafcaab97b195c89
Alexia Pereira <alexia.pereira@cwi.com.br>
2018-08-10 11:09:26 -0300

    feat(login): adicionar tela de login.

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Categorias from 'components/pages/categorias'
 import GastosFixos from 'components/pages/gastosFixos'
 import Estatistica from 'components/pages/estatistica'
 import Inicial from 'components/pages/inicial'
+import Login from 'components/pages/login'
 import OpsGasteiNavbar from 'components/generic/ogNavbar'
 import OpsGasteiMenuBar from 'components/generic/ogMenubar'
 
@@ -11,16 +12,17 @@ export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <OpsGasteiNavbar title="julho" />
+        {/* <OpsGasteiNavbar title="julho" /> */}
         <ScrollView contentContainerStyle={{ flex: 1 }}>
-          <Inicial />
+          <Login />
+          {/* <Inicial /> */}
           {/* <GastosFixos /> */}
           {/* <Estatistica /> */}
           {/* <Categorias /> */}
           {/* <AdicionarObjetivo /> */}
           {/* <Perfil usuario={usuario} salarioAtual={salarioAtual} /> */}
         </ScrollView>
-        <OpsGasteiMenuBar />
+        {/* <OpsGasteiMenuBar /> */}
       </View>
     )
   }
---
+++ src/components/pages/login/loginStyle.js
@@ -0,0 +1,22 @@
+import DEFAULT_STYLES from 'constants/defaultStyles'
+
+export default {
+  ...DEFAULT_STYLES,
+
+  loginContainer: {
+    // width: '100%',
+    // height: '100%',
+    flex: 1,
+  },
+
+  login: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+
+  googleIcon: {
+    width: 200,
+    height: 100,
+  },
+}
