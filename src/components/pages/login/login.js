7667e9731de75a1e5681a5f2a338d38cf6cc7947
Alexia Pereira <alexia.pereira@cwi.com.br>
2018-11-23 13:53:47 -0200

    adicionar google signin

--- src/components/pages/login/login.js
+++ src/components/pages/login/login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Image, View, ImageBackground, TouchableOpacity } from 'react-native'
 import { Actions } from 'react-native-router-flux'
 import { AsyncStorage } from 'react-native'
-import OAuthManager from 'react-native-oauth'
+import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
 
 import { OpsGasteiLogo } from 'components/generic'
 import Background from 'assets/img/back.png'
@@ -24,26 +24,51 @@ class Login extends Component {
   }
 
   async componentDidMount() {
-
-    // this.loadGoogleApi()
-    const manager = new OAuthManager('ops')
-    await manager.configure({
-      google: {
-        // callback_url: 'io.fullstack.FirestackExample:/oauth2redirect',
-        client_id: '898821703398-jh61ekkqvolftqn7fpvomrkki7du6iot.apps.googleusercontent.com',
-        // client_secret: 'YOUR_SECRET',
-      },
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId: '898821703398-jh61ekkqvolftqn7fpvomrkki7du6iot.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      hostedDomain: '', // specifies a hosted domain restriction
+      loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+      forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
+      accountName: 'OpsAndroid', // [Android] specifies an account name on the device that should be used
+      // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+      androidClientId: '898821703398-jh61ekkqvolftqn7fpvomrkki7du6iot.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
     })
+
+    try {
+      await GoogleSignin.hasPlayServices()
+      const userInfo = await GoogleSignin.signIn()
+      console.log(userInfo)
+
+      // this.setState({ userInfo })
+    } catch (error) {
+      console.log(error)
+
+      // if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+      //   // user cancelled the login flow
+      // } else if (error.code === statusCodes.IN_PROGRESS) {
+      //   // operation (f.e. sign in) is in progress already
+      // } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+      //   // play services not available or outdated
+      // } else {
+      //   // some other error happened
+      // }
+    }
+    // this.loadGoogleApi()
+    // const manager = new OAuthManager('ops')
+    // await manager.configure({
+    //   google: {
+    //     // callback_url: 'io.fullstack.FirestackExample:/oauth2redirect',
+    //     client_id: '898821703398-jh61ekkqvolftqn7fpvomrkki7du6iot.apps.googleusercontent.com',
+    //     // client_secret: 'YOUR_SECRET',
+    //   },
+    // })
     // await manager.configure('google', {
     //   // callback_url: 'io.fullstack.FirestackExample:/oauth2redirect',
     //   client_id: '898821703398-jh61ekkqvolftqn7fpvomrkki7du6iot.apps.googleusercontent.com',
     //   client_secret: '898821703398-jh61ekkqvolftqn7fpvomrkki7du6iot.apps.googleusercontent.com',
     // })
-
-    // ...
-    console.log(manager)
-    const resultado = await manager.authorize('google', { scopes: 'profile email' })
-    console.log('cool cool cool cool cool cool cool cool')
   }
 
   async redirect() {
